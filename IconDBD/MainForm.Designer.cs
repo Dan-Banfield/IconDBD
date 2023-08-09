a1607189a0d3ece94f38f98bb61308bf8db6a817
Dan Banfield <73112854+Dan-Banfield@users.noreply.github.com>
2023-08-09 11:44:30 +0100

    Update UI.

--- IconDBD/MainForm.Designer.cs
+++ IconDBD/MainForm.Designer.cs
@@ -28,21 +28,143 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.tabControl = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.installSelectedIconPackButton = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tabControl.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // tabControl
+            // 
+            this.tabControl.Controls.Add(this.tabPage1);
+            this.tabControl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl.Location = new System.Drawing.Point(0, 0);
+            this.tabControl.Name = "tabControl";
+            this.tabControl.SelectedIndex = 0;
+            this.tabControl.Size = new System.Drawing.Size(584, 520);
+            this.tabControl.TabIndex = 0;
+            this.tabControl.TabStop = false;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.button3);
+            this.tabPage1.Controls.Add(this.installSelectedIconPackButton);
+            this.tabPage1.Controls.Add(this.groupBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 30);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(576, 486);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Icon Pack Installer";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(20, 21);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(541, 205);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Icon Pack Selection";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(44, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(453, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "First, select the icon pack you wish to install. It must be a .zip file.";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(48, 91);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(449, 37);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Select Icon Pack";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(44, 149);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(182, 21);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Selected icon pack: None";
+            // 
+            // installSelectedIconPackButton
+            // 
+            this.installSelectedIconPackButton.Location = new System.Drawing.Point(20, 255);
+            this.installSelectedIconPackButton.Name = "installSelectedIconPackButton";
+            this.installSelectedIconPackButton.Size = new System.Drawing.Size(541, 37);
+            this.installSelectedIconPackButton.TabIndex = 3;
+            this.installSelectedIconPackButton.Text = "Install Selected Icon Pack";
+            this.installSelectedIconPackButton.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(20, 308);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(541, 37);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Reset Icons to Default";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(16, 369);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(545, 97);
+            this.label3.TabIndex = 3;
+            this.label3.Text = resources.GetString("label3.Text");
             // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(884, 481);
+            this.ClientSize = new System.Drawing.Size(584, 520);
+            this.Controls.Add(this.tabControl);
+            this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "MainForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "IconDBD";
+            this.tabControl.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl tabControl;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button installSelectedIconPackButton;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- IconDBD/MainForm.cs
+++ IconDBD/MainForm.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.IO;
+using Microsoft.Win32;
+using System.Windows.Forms;
 
 namespace IconDBD
 {
@@ -7,6 +10,31 @@ namespace IconDBD
         public MainForm()
         {
             InitializeComponent();
+        }
+
+        private string GetDBDIconPath()
+        {
+            string steamInstallPath = GetSteamInstallPath(Environment.Is64BitOperatingSystem);
+
+            if (!string.IsNullOrEmpty(steamInstallPath))
+            {
+                string iconPath = Path.Combine(steamInstallPath, "steamapps", "common", "Dead by Daylight", "DeadByDaylight", "Content", "UI", "Icons");
+
+                if (Directory.Exists(iconPath))
+                    return iconPath;
+            }
+            return "";
+        }
+
+        private string GetSteamInstallPath(bool is64Bit)
+        {
+            string installPath = "";
+            using (RegistryKey key = Registry.LocalMachine.OpenSubKey(is64Bit ? @"SOFTWARE\Wow6432Node\Valve\Steam" : @"SOFTWARE\Valve\Steam"))
+            {
+                if (key != null)
+                    installPath = (string)key.GetValue("InstallPath");
+            }
+            return installPath;
         }
     }
 }
